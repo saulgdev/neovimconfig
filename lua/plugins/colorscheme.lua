6d9163fbb827cd7234496522f6b408204a51967a
Saul Gomes <saul.gomes@ng7consultoria.com.br>
2023-12-11 13:14:28 -0300

    feat
    more themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -41,10 +41,13 @@ return {
       },
     },
   },
+  { "navarasu/onedark.nvim", opts = {
+    style = "darker",
+  } },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "onedark",
     },
   },
 }
