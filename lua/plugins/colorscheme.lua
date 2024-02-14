0b0fe7cbe01aed671459c60edd5129f20c908dc1
Saul Gomes <saul.contatodev@gmail.com>
2024-02-14 13:24:13 -0300

    fix: remove kanagawa

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,32 +1,5 @@
 return {
     {
-        "rebelot/kanagawa.nvim",
-        opts = {
-            compile = false, -- enable compiling the colorscheme
-            undercurl = true, -- enable undercurls
-            commentStyle = { italic = true },
-            keywordStyle = { italic = true },
-            functionStyle = {},
-            -- statementStyle = { bold = true },
-            typeStyle = {},
-            transparent = false, -- do not set background color
-            dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-            terminalColors = true, -- define vim.g.terminal_color_{0,17}
-            colors = { -- add/modify theme and palette colors
-                palette = {},
-                theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-            },
-            overrides = function(colors) -- add/modify highlights
-                return {}
-            end,
-            theme = "wave", -- Load "wave" theme when 'background' option is not set
-            background = { -- map the value of 'background' option to a theme
-                dark = "wave", -- try "dragon" !
-                light = "lotus",
-            },
-        },
-    },
-    {
         "navarasu/onedark.nvim",
         opts = {
             style = "warmer", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
