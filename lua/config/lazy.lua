05f8f8aae277ef16ff3bf2644587f74da8bdbc44
Saul Gomes <saul.contatodev@gmail.com>
2024-03-23 09:15:39 -0300

    chore: enable default extras configs like docker and yaml

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,6 +22,8 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.ui.mini-animate" },
         { import = "lazyvim.plugins.extras.dap.core" },
         { import = "lazyvim.plugins.extras.lang.rust" },
+        { import = "lazyvim.plugins.extras.lang.yaml" },
+        { import = "lazyvim.plugins.extras.lang.docker" },
         -- { import = "lazyvim.plugins.extras.formatting.black" },
         -- { import = "lazyvim.plugins.extras.coding.copilot" },
         { import = "plugins" },
